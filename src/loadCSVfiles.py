57d1638e1c961a7a4cae906b7fd402596ccca69a
SaxenaSim <saxenasimran84@gmail.com>
2024-05-28 11:16:18 +0530

    modify loadCSVfile.py

--- src/loadCSVfiles.py
+++ src/loadCSVfiles.py
@@ -11,24 +11,25 @@ class CSVLoader:
     @staticmethod
     def load_csv_data(tx):
         logger = Logger.getlogger()
-        logger.debug("entering into loader method")
+        logger.debug("entering into load_csv_data method")
         # Loading consumers data
         query = """
         LOAD CSV WITH HEADERS FROM 'file:///home/user1/Desktop/neo4jGraphDatabase/data/consumers.csv' AS row
         MERGE (c:Consumers {consumer_id: toInteger(row.consumer_id)})
         ON CREATE SET c.name = row.name, c.age = toInteger(row.age), c.email = row.email, c.gender = row.gender , c.business_id = toInteger(row.business_id);
         """
-        logger.debug("query")
+        logger.info(f"Running query:{query}")
         results = tx.run(query)
-        logger.info(f"Executed Consumer query {results}")
+        logger.info(f"Executed Consumer query with results: {results}")
         # Loading business data
         query = """
         LOAD CSV WITH HEADERS FROM 'file:///home/user1/Desktop/neo4jGraphDatabase/data/businesses.csv' AS row
         MERGE (b:Businesses {business_id: toInteger(row.business_id)})
         ON CREATE SET b.name = row.name, b.domain_purchased = row.domain_purchased;
         """
-        tx.run(query)
-        logger.info("Executed Business query")
+        logger.info(f"Running query:{query}")
+        results=tx.run(query)
+        logger.info(f"Executed Business query with result:{results}")
         # Creating relationships between consumers and businesses
         query = """
         MATCH (c:Consumers), (b:Businesses)
