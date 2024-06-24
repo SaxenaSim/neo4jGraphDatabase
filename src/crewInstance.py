89fa3dfa6db2d309478331e4641ddf54e30717b2
SaxenaSim <saxenasimran84@gmail.com>
2024-06-24 11:23:09 +0530

    add validation

--- src/crewInstance.py
+++ src/crewInstance.py
@@ -1,5 +1,6 @@
 from queryExecutionAgent import queryExecutionAgent as qe
 from queryGenerationAgent import queryGenerationAgent as qg
+from queryValidationAgent import queryValidationAgent as qv
 from crewai import Crew
 
 
@@ -11,14 +12,18 @@ class CrewInstance:
     def initialize(cls):
         obj_qe = qe()
         obj_qg = qg()
+        obj_qv = qv()
         creation_agent = obj_qg.queryAgent()
         creation_task = obj_qg.queryTask(creation_agent)
         execution_agent = obj_qe.executionAgent()
         execution_task = obj_qe.executionTask(execution_agent)
+        validate_agent = obj_qv.ValidateAgent()
+        validate_task = obj_qv.ValidateTask(validate_agent)
+        
         
         cls.crew_instance = Crew(
-                    agents=[creation_agent, execution_agent],
-                    tasks=[creation_task, execution_task],
+                    agents=[creation_agent,validate_agent, execution_agent],
+                    tasks=[creation_task,validate_task, execution_task],
                     verbose=True,
                     memory=True,
                 )
